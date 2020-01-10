4069016717b96a387f0e849a9897991a4513aeae
Lauren Yu <laurenkyu@gmail.com>
2020-01-10 16:15:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  create_an_array = [ ]
+  empty_array = [ ]
 end
 
 def create_an_array
-  
+  colors = ["blue", "red", "green", "yellow"]
 end
 
 def add_element_to_end_of_array(array, element)
