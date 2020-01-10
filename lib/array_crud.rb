6832a85a45f2c8bbb22178b81cac8d783dbd8360
Lauren Yu <laurenkyu@gmail.com>
2020-01-10 16:16:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  colors = ["blue", "red", "green", "yellow"]
+  colors << "purple"
 end
 
 def add_element_to_start_of_array(array, element)
